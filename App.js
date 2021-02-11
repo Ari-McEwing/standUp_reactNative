d639220c3485fc1649763423ec04437198fe5835
Ari Mc Ewing <U758065@DOW.COM>
2021-02-10 22:13:38 -0600

    First Commit

--- App.js
+++ App.js
@@ -5,9 +5,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>we did it boys</Text>
       <StatusBar style="auto" />
-    </View>
+      </View>
   );
 }
 
